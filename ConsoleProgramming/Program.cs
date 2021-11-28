4001f71974868e57f0045df90be7c95f34315438
= <acikgoze18@itu.edu.tr>
2021-11-28 15:53:04 +0300

    Initial commit

---
+++ ConsoleProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter your name ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Surname: ");
+            var sname = Console.ReadLine();
+            Console.WriteLine("Hello, "+ name + " "+ sname);
+        }
+    }
+}
